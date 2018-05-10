eada0430ce8c973e0fdeccd2452bbce64d1c1dbb
Kravtsiv Yura <kravtsivyura@gmail.com>
2018-05-10 12:09:56 +0300

    withoutKey

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).load(function(){
+var map;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+});
+
+
+
+
