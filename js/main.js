fa82e74cde8677b8afb0f5e6d4d649e921019a7f
Kravtsiv Yura <kravtsivyura@gmail.com>
2018-05-18 19:53:56 +0300

    probation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-$(window).load(function(){
-var map;
-
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-});
-
-
-
 
